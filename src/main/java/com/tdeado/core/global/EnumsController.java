69a67e7cfa2dec0e2b66fb8d6fb070349c961616
yangzhe <yancy@tdeado.com>
2022-11-10 13:36:41 +0800

    添加Violation参数验证

--- src/main/java/com/tdeado/core/global/EnumsController.java
+++ src/main/java/com/tdeado/core/global/EnumsController.java
@@ -11,15 +11,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 公共枚举接口
+ */
 @RestController
 @RequestMapping("enum")
 public class EnumsController {
     Map<String, List<Object>> enums = new HashMap<>();
+
+    /**
+     * 初始化枚举
+     */
     public EnumsController() {
         for (Class<?> aClass : ClassUtil.scanPackageBySuper("com.tdeado", Enum.class)) {
             enums.put(aClass.getSimpleName(), EnumUtil.getFieldValues((Class<? extends Enum<?>>) aClass,"label"));
         }
     }
+
+    /**
+     * 获取枚举数据
+     * @param name 枚举类名
+     * @return 枚举数据结果
+     */
     @RequestMapping("{name}")
     public R<List<Object>> enumInfo(@PathVariable String name){
         return R.ok(enums.get(name));
